2adbd6418d84335ca5729f2074ea299b63cfe13b
Kanak Gautam <81749791+kanak2106@users.noreply.github.com>
2021-12-07 10:14:11 +0530

    Add files via upload

---
+++ atm.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+void showMenu()
+{
+    cout<<"**********Menu**********"<< endl;
+    cout<<"1. Check Balance"<< endl;
+    cout<<"2. Deposit"<< endl;
+    cout<<"3. Withdraw"<< endl;
+    cout<<"4. Exit"<< endl;
+    cout<<"************************"<< endl;
+
+}
+
+int main()
+{   int option;
+double balance = 500;
+do
+{
+    showMenu(); 
+
+    cout<<"Option: "<< endl;
+
+    cin>> option;
+    switch (option)
+    {
+    case 1: cout<< "Balance is "<< balance<<" Rs"<< endl; 
+    break;
+
+
+
+
+    case 2: cout<< "Deposit amount: ";
+
+    double depositAmount;
+    cin>> depositAmount;
+
+    balance += depositAmount;
+    break;
+
+
+
+
+    case 3: cout<< "Withdraw amount ";
+
+    double withdrawAmount;
+    cin>> withdrawAmount;
+    if(balance>= withdrawAmount)
+    balance -= withdrawAmount;
+
+    else
+    cout<< "Not enough money";
+    break;
+
+
+    }
+} while (option!=4);
+
+
+    
+    system("pause>0");
+    
+ return 0;
+}
